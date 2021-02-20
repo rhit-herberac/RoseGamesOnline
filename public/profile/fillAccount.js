9975abc38833d934bc0bc6c14eaf4e15b6b101ff
buczkob1 <buczkob1@rose-hulman.edu>
2021-02-19 19:40:41 -0500

    Got my games to work. Now will call games stored in firestore

--- public/profile/fillAccount.js
+++ public/profile/fillAccount.js
@@ -1,14 +1,21 @@
 const USERS_COLLECTION = 'Users';
+const GAME_COLLECTION = 'GameMetadata';
+
+if (document.querySelector("#mainPage")) {
+    browse_param();
+}
+
+
 var _ref = firebase.firestore().collection(USERS_COLLECTION);
 let _documentSnapshot;
 let ran = false;
-let _unsubscribe = this._ref.orderBy("Email", "desc").limit(50	).onSnapshot((querySnapshot) => {
+let _unsubscribe = _ref.orderBy("Email", "desc").limit(50	).onSnapshot((querySnapshot) => {
     console.log("User update!");
     querySnapshot.forEach((doc) => {
         let data = doc.data();
         let segment = doc.Ef.path.segments;
         let uid = segment[segment.length - 1];
-        if (uid == signInController.getUID()) updateProfile(data);
+        if (uid == signInController.getUID() && document.querySelector("#profileContainer")) updateProfile(data);
     });
 });
 
@@ -17,10 +24,37 @@ function updateProfile(data) {
     document.querySelector("#usernameDisplay").innerHTML = data.Name;
 }
 
-// const ref = firebase.firestore().collection("Users");
-// ref.onSnapshot((querySnapshot) =>{
+function browse_param() {
+    document.querySelector("#mainPage").innerHTML = '';
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const uid = urlParams.get('uid');
+    var _ref = firebase.firestore().collection(GAME_COLLECTION);
+    let _unsubscribe = _ref.onSnapshot((querySnapshot) => {
+        console.log("Game update!");
+        querySnapshot.forEach((doc) => {
+            if (uid) {
+                let cur = doc;
+                let data = doc.data();
+                let auth = data.Author;
+                if (auth) {
+                    let seg = auth.Ef.path.segments;
+                    let id = seg[seg.length - 1];
+                    if (id == uid) addGame(data, cur);
+                }
+            } else {
+                let cur = doc;
+                let data = doc.data();
+                addGame(data, cur);
+            }
 
-//     querySnapshot.forEach((doc) => {
-//         console.log(doc.data());
-//     });
-// });
+        });
+    });
+}
+
+function addGame(data, cur) {
+    console.log(data);
+    let seg = cur.Ef.path.segments;
+    let id = seg[seg.length - 1];
+    document.querySelector("#mainPage").innerHTML += `<a href="../game/?id=${id}">${data.Title}</a>`
+}
--- public/scripts/assignButtons.js
+++ public/scripts/assignButtons.js
@@ -1,8 +1,26 @@
 function main() {
 
-    document.querySelector("#friendsButton").onclick = (event) => {
-        window.location.href = "friends.html";
+    if (document.querySelector("#friendsButton")) {
+        document.querySelector("#friendsButton").onclick = (event) => {
+            window.location.href = "friends.html";
+        }
+    }
+
+    if (document.querySelector("#addFriendButton")) {
+        document.querySelector("#addFriendButton").onclick = (event) => {
+            console.log("todo");
+        }
+    }
+
+    if (document.querySelector("#myGames")) {
+        console.log("here");
+        document.querySelector("#myGames").onclick = (event) => {
+            console.log("todo");
+            window.location.href = `../browse/?uid=${signInController.getUID()}`;
+        }
     }
 }
 
-main();
+setTimeout(() => {
+    main();
+}, 500);
